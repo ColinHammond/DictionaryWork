7e2bbfa9e6b81b9d5fd599aa9c9c1fb025ef0d81
ColinHammond <c2hammond@student.bridgew.edu>
2022-10-17 20:02:51 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+afc_east_west_wins = {'Bills': 4, 'Jets': 3, 'Dolphins': 3, 'Patriots': 2, 'Chiefs': 3,
+                      'fantasy team': 8, 'Chargers': 3, 'Broncos': 2,
+                      'Comp151 Computers': 11, 'Raiders': 1}
+
+
+
+
+# below replaces a value that is lesser than a current value with a different value
+#largest_so_far = 'Raiders'
+#for team in afc_east_west_wins.keys():
+   # if afc_east_west_wins[largest_so_far] < afc_east_west_wins[team]:
+# above: if largest so far is less than current team
+       # largest_so_far = team
+# above: replaces previous value to current team value
+# if the current team has more wins than the previous one
+#print(f" The team with the most wins is {largest_so_far}\ with {afc_east_west_wins[largest_so_far]} wins")
+
+
+
+#below lists the team names and how many wins they each have
+#for team_name in afc_east_west_wins.keys():
+    #print(f"The {team_name} have {afc_east_west_wins[team_name]} wins so far")
